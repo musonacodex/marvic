85f90f7d52e9f08b8a7b868e8dd2d1c5037d5f3e
musonacodex <musonacodex@gmail.com>
2025-09-02 08:59:36 +0100

    perf: Improve the performance of Marvic\Settings instance methods

--- src/Marvic/Settings.php
+++ src/Marvic/Settings.php
@@ -67,26 +67,9 @@ final class Settings {
 	 * @param  string $key
 	 * @return string
 	 */
-	private function validateKey(string $key): string {
+	private function validate(string $key): string {
 		if ( !preg_match('/^[a-zA-Z_][a-zA-Z0-9_.]$/', $key) ) return $key;
 		throw new InvalidArgumentException("Invalid setting key: $key");
-	}
-
-	/**
-	 * Check if a setting data exists.
-	 * 
-	 * @param  string  $key
-	 * @return boolean
-	 */
-	public function has(string $key): bool {
-		$this->validateKey($key);
-		[$data, $parts] = [$this->data, explode('.', $key)];
-
-		foreach ($parts as $part) {
-			if ( !isset($data[$part]) ) return false;
-			$data = $data[is_numeric($part) ? (int)$part : $part];
-		}
-		return true;
 	}
 
 	/**
@@ -97,12 +80,12 @@ final class Settings {
 	 * @return mixed
 	 */
 	public function get(string $key, mixed $default = null): mixed {
-		if ( !$this->has($key) ) return $default;
-
-		[$data, $parts] = [$this->data, explode('.', $key)];
-
-		foreach ($parts as $part)
+		$this->validate($key);
+		$data = $this->data;
+		foreach (explode('.', $key) as $part) {
+			if ( !isset($data[$part]) ) return $default;
 			$data = $data[is_numeric($part) ? (int)$part : $part];
+		}
 		return $data ?? $default;
 	}
 
@@ -113,17 +96,28 @@ final class Settings {
 	 * @param mixed  $value
 	 */
 	public function set(string $key, mixed $value): void {
-		$this->validateKey($key);
+		$this->validate($key);
 		$callback = function(&$data, $parts, $value) use (&$callback) {
 			$part = array_shift($parts);
 			if ($part === null) { $data = $value; return; }
 			$part = is_numeric($part) ? (int)$part : $part;
 
-			if ( !isset($data[$part]) ) $data[$part] = [];;
+			if ( !isset($data[$part]) ) $data[$part] = [];
 
 			$callback($data[$part], $parts, $value);
 		};
 		$callback($this->data, explode('.', $key), $value);
+	}
+
+	/**
+	 * Check if a setting data exists.
+	 * 
+	 * @param  string  $key
+	 * @return boolean
+	 */
+	public function has(string $key): bool {
+		$token = '__MARVIC_APP_SETTINGS_KEY_NOT_FOUND__';
+		return $this->get($key, $token) !== $token;
 	}
 
 	/**
@@ -161,7 +155,7 @@ final class Settings {
 	 * 
 	 * @param self $settings
 	 */
-	public function merge(self $settings): void {
-		$this->data = array_merge($this->data, $settings->all());
+	public function merge(array $settings = []): void {
+		$this->data = array_merge($this->data, $settings);
 	}
 }
