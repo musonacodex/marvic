4a7f57006c242b723fda971fc29fa645ccad3f58
musonacodex <musonacodex@gmail.com>
2025-09-02 11:11:03 +0100

    docs: Doc the Marvic\Application class

--- src/Marvic/Application.php
+++ src/Marvic/Application.php
@@ -12,11 +12,18 @@ use Marvic\HTTP\Message\Response;
 use Marvic\HTTP\Message\Response\Status;
 
 /**
+ * Marvic Application Class
+ *
+ * This class represents an Marvic Web Application instance. An
+ * Application can exists singly or to be inside of an application tree.
+ * Every application is mount on a specifical URL path
+ * 
  * @package Marvic\Core
- * @version 1.0.0
  */
 final class Application {
 	/** 
+	 * Base url path of application where it is mount.
+	 * 
 	 * @var string
 	 */
 	public string $mountpath = '/';
@@ -27,28 +34,43 @@ final class Application {
 	public array $context = [];
 
 	/**
+	 * Application parent reference on an application tree.
+	 * 
 	 * @var self
 	 */
 	public readonly ?self $parent;
 
 	/**
+	 * The Marvic Application Settings Instance.
+	 * 
 	 * @var Marvic\Core\Settings
 	 */
 	public readonly Settings $settings;
 
 	/**
+	 * The Marvic Application Router Instance.
+	 * 
 	 * @var Marvic\Routing\Router
 	 */
 	public readonly Router $router;
 
-
+	/**
+	 * The Instance Constructor Method.
+	 * 
+	 * @param Marvic\Settings $settings
+	 */
 	public function __construct(Settings $settings) {
 		$this->settings = $settings;
 		$this->router   = new Router();
 	}
 
+	/**
+	 * Return the string representation of Instance.
+	 * 
+	 * @return string
+	 */
 	public function __toString(): string {
-		return "<Application mount in '$this->mountpath'>";
+		return "<Application mount on '$this->mountpath'>";
 	}
 
 	public function __call(string $name, array $arguments = []): mixed {
@@ -57,6 +79,11 @@ final class Application {
 		return call_user_func_array([$this->router, $name], $arguments);
 	}
 
+	/**
+	 * Define and set the application environment.
+	 * 
+	 * @param string $env
+	 */
 	private function defineEnvironment(string $env = 'development'): void {
 		switch ( $env ) {
 			case 'development':
@@ -79,16 +106,36 @@ final class Application {
 		}
 	}
 
+	/**
+	 * Get an application settings data. If the settings key is not
+	 * valid, add a new route with method GET.
+	 * 
+	 * @param  array $arguments
+	 * @return mixed
+	 */
 	public function get(...$arguments): mixed {
 		if ( preg_match('/^[a-zA-Z_][a-zA-Z0-9_.]*$/', $arguments[0]) )
 			return $this->settings->get($arguments[0], $arguments[1] ?? null);
 		return $this->__call('get', $arguments);
 	}
 
+	/**
+	 * Set an application settings data.
+	 * 
+	 * @param string $key
+	 * @param mixed  $value
+	 */
 	public function set(string $key, mixed $value): void {
 		$this->settings->set($key, $value);
 	}
 
+	/**
+	 * Use a middleware and install it on an URL path using a router.
+	 * If the argument contains an application, or group of them, they
+	 * will be childs of this application.
+	 * 
+	 * @param array $arguments
+	 */
 	public function use(...$arguments): void {
 		$this->router->use(...$arguments);
 	}
@@ -97,6 +144,9 @@ final class Application {
 		$this->settings->set("engines.$name", $engine);
 	}
 
+	/**
+	 * Run the web application, pr finish if the type of intarface is CLI.
+	 */
 	public function run(?Callable $ondone = null): void {
 		if ( defined('PHP_SAPI') && PHP_SAPI === 'cli' ) return;
 
