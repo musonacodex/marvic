04badb75ca3ca71a64224b94b3c4686c3b82aaa7
musonacodex <musonacodex@gmail.com>
2025-09-03 08:52:57 +0100

    docs: Doc the Marvic\HTTP\Message\Request\Method class

--- src/Marvic/HTTP/Message/Request/Methods.php
+++ src/Marvic/HTTP/Message/Request/Methods.php
@@ -5,27 +5,48 @@ namespace Marvic\HTTP\Message\Request;
 /**
  * List of supported HTTP request methods by the Marvic Framework.
  * 
- * @package Marvic\JTTP
- * @version 1.0.0
+ * @package Marvic\HTTP\Message\Request
  */
 final class Methods {
-	/** @var string[] */
+	/** 
+	 * List of supported HTTP methods.
+	 */
 	private const COLLECTION = [
 		'GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS', 'CONNECT', 'TRACE'
 	];
 
+	/** 
+	 * List of idempotent HTTP methods.
+	 */
 	private const IDEMPOTENT = [
 		'GET', 'HEAD', 'PUT', 'DELETE', 'OPTIONS', 'TRACE'
 	];
 
+	/**
+	 * Check if the HTTP method exists on the list.
+	 * 
+	 * @param  string  $method
+	 * @return boolean
+	 */
 	public static function has(string $method): bool {
 		return in_array(strtoupper($method), self::COLLECTION);
 	}
 
+	/**
+	 * Check if the HTTP method is idempotent.
+	 * 
+	 * @param  string  $method
+	 * @return boolean
+	 */
 	public static function idempotent(string $method): bool {
 		return in_array(strtoupper($method), self::IDEMPOTENT);
 	}
 
+	/**
+	 * Return all HTTP methods.
+	 * 
+	 * @return arrau
+	 */
 	public static function all(): array {
 		return self::COLLECTION;
 	}
