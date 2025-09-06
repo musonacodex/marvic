157ca007b337e183498d3a87a52058246f3a6254
musonacodex <musonacodex@gmail.com>
2025-09-06 19:15:47 +0100

    feat: Add the support of multiple handlers by route in different methods.

--- src/Marvic/Routing/Route.php
+++ src/Marvic/Routing/Route.php
@@ -4,110 +4,142 @@ namespace Marvic\Routing;
 
 use Exception;
 use ReflectionFunction;
+use InvalidArgumentException;
 use Marvic\HTTP\Message\Request;
+use Marvic\HTTP\Message\Request\Methods;
 use Marvic\HTTP\Message\Response;
 
 /**
  * Marvic Application Route.
  * 
- * This class represents an application route, a path that defines how
- * an application responds to a request made to a given address (URL). 
- * A route store a path, a method and a handler.
+ * This class represents an route, used to manage routes of an
+ * application. Each route is associated by a path and handlers that
+ * they handle in different HTTP methods.
  *
  * @package Marvic\Routing
  */
 final class Route {
 	/**
-	 * A route method.
-	 * 
-	 * @var string
-	 */
-	public readonly string $method;
-
-	/**
-	 * A route path.
+	 * The route path with support of parameters.
 	 * 
 	 * @var string
 	 */
 	public readonly string $path;
 
 	/**
-	 * A route handler.
-	 * 
-	 * @var Callable
+	 * The route matcher.
+	 *
+	 * @var Marvic\Routing\RouteMatcher
 	 */
-	private readonly object $handler;
+	public readonly RouteMatcher $matcher;
 
 	/**
-	 * Is the route handler a middleware?
+	 * The relationship between a method and your handler stack.
 	 * 
-	 * @var boolean
+	 * @var array<string, list>
 	 */
-	public readonly bool $isMiddleware;
+	private array $stacks = [];
 
 	/**
 	 * The Instance Constructor Method.
 	 * 
-	 * @param string   $method
-	 * @param string   $path
-	 * @param Callable $handler
-	 * @param boolean  $isMiddleware
+	 * @param string $path
 	 */
-	public function __construct(string $method, string $path, Callable $handler,
-		bool $isMiddleware = false)
-	{
-		$this->path         = $path;
-		$this->method       = $method;
-		$this->handler      = $handler;
-		$this->isMiddleware = $isMiddleware; 
+	public function __construct(string $path, RouteMatcher $matcher) {
+		$this->path    = $path;
+		$this->matcher = $matcher;
 	}
 
-	/**
-	 * Run the handler function to handle HTTP messages.
-	 * 
-	 * @param  Marvic\HTTP\Message\Request  $req
-	 * @param  Marvic\HTTP\Message\Response $res
-	 * @param  Callable                     $next
-	 */
-	public function handleRequest(Request $req, Response $res, Callable $next): void {
-		$callback   = $this->handler;
-		$reflection = new ReflectionFunction($callback);
-		$parameters = array_column($reflection->getParameters(), 'name');
-		
-		if ( count($parameters) > 3 ) { $next(); return; }
+	public function __call(string $name, array $arguments): self {
+		if ($name !== strtolower($name) || !Methods::has(strtoupper($name))) {
+			$message = "Invalid route method: $name $this->path";
+			throw new Exception($message);
+		}
+		$name = strtoupper($name);
+		if (! array_key_exists($name, $this->stacks) ) $this->stacks[$name] = [];
+		$handlers = array_map([$this, 'validateHandler'], $arguments);
+		array_push($this->stacks[$name], ...$handlers);
+		return $this;
+	}
 
+	private function validateHandler($handler) {
+		if (! is_callable($handler) ) {
+			$message = "Invalid route handler: $this->path";
+			throw new InvalidArgumentException($message);
+		}
+		$reflection = new ReflectionFunction($handler);
+		$patameters = $reflection->getParameters();
+		
+		if ( empty($reflection->getParameters()) ) {
+			$message = "Handler arguments is required";
+			throw new InvalidArgumentException($message);
+		}
+		return $handler;
+	}
+
+	public function handlesMethod(string $method): bool {
+		return in_array($method, array_keys($this->stacks));
+	}
+
+	public function match(array $methods, ...$handlers): self {
+		foreach (array_map('strtolower', $methods) as $method)
+			$this->{$method}(...$handlers);
+		return $this;
+	}
+
+	public function any(...$handlers): self {
+		return $this->match(Methods::all(), ...$handlers);
+	}
+
+	public function view(string $name, array $data = []): self {
+		return $this->get(function($req, $res) use ($name, $data) {
+			$response->render($name, $data);
+		});
+	}
+	
+	public function redirect(string $uri, int $status = 302): self {
+		return $this->get(function($req, $res) use ($uri, $status) {
+			$response->redirect($uri, $status);
+		});
+	}
+
+	public function handleRequest(Callable $handler, Request $req, Response $res,
+		Callable $next): void {
 		try {
-			$result = call_user_func_array($callback, [$req, $res, $next]);
-			if (! is_null($result) ) $res->send($result);
+			$parameters = [$req, $res, $next];
+			call_user_func_array($handler, $parameters);
 			
 		} catch (Exception $error) {
 			$next($error);
 		}
 	}
 
-	/**
-	 * Run the handler function to handle HTTP message errors.
-	 * 
-	 * @param  mixed                        $error
-	 * @param  Marvic\HTTP\Message\Request  $req
-	 * @param  Marvic\HTTP\Message\Response $res
-	 * @param  Callable                     $next
-	 */
-	public function handleError(mixed $error, Request $req, Response $res,
-		Callable $next): void
-	{
-		$callback   = $this->handler;
-		$reflection = new ReflectionFunction($callback);
-		$parameters = array_column($reflection->getParameters(), 'name');
-		
-		if ( count($parameters) !== 4 ) { $next($error); return; }
-
+	public function handleError(Callable $handler, mixed $error, Request $req,
+		Response $res, Callable $next): void {
 		try {
-			$result = call_user_func_array($callback, [$error, $req, $res, $next]);
-			if (! is_null($result) ) $res->send($result);
+			$parameters = [$error, $req, $res, $next];
+			call_user_func_array($handler, $parameters);
 			
 		} catch (Exception $otherError) {
 			$next($otherError);
 		}
+	}
+
+	public function dispatch(Request $req, Response $res, Callable $done): void {
+		$index = -1;
+		$stack = $this->stacks[$req->method];
+
+		if ( empty($stack) ) { $done(); return; }
+
+		$next = function($error = null) use (&$next, &$stack, $index, $done, $req, $res) {
+			if ( in_array($error, ['route', 'router']) ) return $done();
+			if ( $index >= count($stack) ) return $done();
+
+			$handler = $stack[++$index];
+
+			if ( $error ) $this->handleError($handler, $error, $req, $res, $next);
+			else $this->handleRequest($handler, $req, $res, $next);
+		};
+		$next();
 	}
 }
