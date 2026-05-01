b0027108c680b17a4eb152b2e4c055699dfcbdcb
musonacodex <musonacodex@gmail.com>
2026-05-01 22:10:35 +0100

    feat(middleware): add Marvic::router() factory method

--- src/Marvic.php
+++ src/Marvic.php
@@ -1,5 +1,12 @@
 <?php
 
-final class Marvic {
+use Marvic\Http\Router;
+use Marvic\Http\Message\Request;
+use Marvic\Http\Message\Request\Methods;
+use Marvic\Http\Message\Response;
 
+final class Marvic {
+	public static function router($options): Router {
+		return new Router($options);
+	}
 }
