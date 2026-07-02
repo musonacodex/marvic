0ca6777a68c74f90618c7715a5c2cd8f8aa2732c
musonacodex <musonacodex@gmail.com>
2026-07-02 12:49:57 +0100

    docs(example): add helloworld example

---
+++ examples/helloworld/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once './../../vendor/autoload.php';
+
+$app = Marvic::application();
+
+$app->get('/', function($request, $response) {
+	$response->send('Hello World!');
+});
+
+echo $app->run();
